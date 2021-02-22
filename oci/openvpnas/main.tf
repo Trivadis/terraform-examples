e9e9a62781e59d531070ae67f55733938d4e5ff6
Stefan Oehrli <stefan.oehrli@trivadis.com>
2021-02-22 10:41:05 +0100

    terraform fmt

--- oci/openvpnas/main.tf
+++ oci/openvpnas/main.tf
@@ -16,10 +16,10 @@
 
 # create compartment --------------------------------------------------------
 resource "oci_identity_compartment" "tf-compartment" {
-    # Required
-    compartment_id = var.compartment_master_ocid
-    description = var.compartment_description
-    name = var.compartment_name
+  # Required
+  compartment_id = var.compartment_master_ocid
+  description    = var.compartment_description
+  name           = var.compartment_name
 }
 
 # verify availability domain -------------------------------------------------
