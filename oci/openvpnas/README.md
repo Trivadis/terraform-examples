6c21592ab95c0e38df6a1718d529c993b0a36fcb
martinberger-ch <56437524+martinberger-ch@users.noreply.github.com>
2021-02-22 10:32:07 +0100

    New Terrform Code for OpenVPN - Initial Upload

---
+++ oci/openvpnas/README.md
@@ -0,0 +1,81 @@
+# OpenVPN Setup with Terraform
+This example contains a Terraform configuration to a public/private subnet setup and an OpenVPN server up and running. Part of the OpenVPN provisioning code is reverse engineered from the Oracle Cloud Infrastructure OCI Resource Manager stack.
+
+![OCI Architecture Picture](image/oci_small_dev_architecture.jpg)  
+
+# What this example covers
+ * A new compartment is created based on an existing compartment
+ * VCN with regional public and private subnet provisioned
+ * Routing tables and security lists provisioned
+ * Internet gateway, NAT gateway and service gateway provisioned
+ * OpenVPN Access Server from OCI Marketplace in Public Subnet- Free for 2 Connections
+ * Oracle Linux Server in private Subnet for further actions like using as webserver, etc.
+
+# Prerequisites
+  * Oracle OCI CLI installed and configured 
+  * Terraform up and running
+  * Git client installed
+  * SSH public key for OCI instance setup available
+
+
+# Variables in terraform.tfvars file
+Required variables according your Oracle Cloud Infrastructure environment.
+| Variable                              | Value                                      |
+|---------------------------------------|--------------------------------------------|
+| TF_VAR_user_ocid                      | User OCID                                  | 
+| TF_VAR_fingerprint                    | OCI API Fingerprint                        |
+| TF_VAR_key_file                       | OCI API SSH Key File                       |
+| TF_VAR_tenancy_ocid                   | Tenancy OCID                               |
+| TF_region                             | OCI Region                                 |
+| TF_VAR_compartment_name               | Compartment Name                           |
+| TF_VAR_compartment_description        | Compartment Description                    |
+| TF_VAR_compartment_master_ocid        | Master Compartment OCID                    |
+| TF_VAR_compute_ssh_authorized_keys    | SSH Public Key for Compute Instance Access |
+| TF_VAR_openvpn_admin_password         | OpenVPN inital login password              |
+
+
+## Example to export specific variables:
+```bash
+export TF_VAR_tenancy_ocid=<your_tenancy_ocid>
+export TF_VAR_user_ocid=<your_username_OCID>                              
+export TF_VAR_private_key_path=<your_ssh_private_key>   
+export TF_VAR_fingerprint=<your_public_key_fingerprint>
+export TF_VAR_region=<your_OCI_region>                           
+export TF_VAR_compartment_name=<your_compartment_name>
+export TF_VAR_compartment_description=<your_compartment_description>
+export TF_VAR_compartment_master_ocid=<your_OCID of the master compartment>
+export TF_VAR_compute_ssh_authorized_keys=<your_public_SSH_key_for_instance_access>
+export TF_VAR_openvpn_admin_password=<your_openvpn_inital_password_for_user_openvpnadmin>
+```
+
+# Usage
+
+ * Define the provider specific variables either in terraform.tfvars or via ENV variables
+ * Initialize the OCI terraform provider
+
+
+## Terraform init
+
+```bash
+$ terraform init
+```
+
+## Terraform plan
+
+```bash
+$ terraform plan -out=openvpn.tfplan
+```
+
+## Terraform apply
+```bash
+$ terraform apply openvpn.tfplan
+```
+
+# OpenVPN URL
+The public IP address is part of the Terraform output. To get the OVPN client configuration file:
+ * https://<your_openvpnas_public_ip_here>/?=connect
+
+ Admin console
+ * https://<your_openvpnas_public_ip_here>/admin
+
+
---
+++ oci/openvpnas/variables.tf
@@ -0,0 +1,201 @@
+# ---------------------------------------------------------------------------
+# Trivadis AG, Infrastructure Managed Services
+# Saegereistrasse 29, 8152 Glattbrugg, Switzerland
+# ---------------------------------------------------------------------------
+# Name.......: variables.tf
+# Author.....: Martin Berger (mbg) martin.berger@trivadis.com
+# Editor.....: Martin Berger
+# Date.......: 2021.02.11
+# Revision...: 
+# Purpose....: Variable definition for this terraform configuration.
+# Notes......: --
+# Reference..: --
+# License....: Apache License Version 2.0, January 2004 as shown
+#              at http://www.apache.org/licenses/
+# ---------------------------------------------------------------------------
+
+# provider identity parameters ----------------------------------------------
+variable "user_ocid" {
+  description = "user OCID used to access OCI"
+  type        = string
+}
+variable "fingerprint" {
+  description = "Fingerprint for user"
+  type        = string
+}
+
+variable "private_key_path" {
+  description = "Private Key Path"
+  type        = string
+}
+
+variable "tenancy_ocid" {
+  description = "tenancy id where to create the resources"
+  type        = string
+}
+
+variable "region" {
+  # List of regions: https://docs.cloud.oracle.com/iaas/Content/General/Concepts/regions.htm#ServiceAvailabilityAcrossRegions
+  description = "The OCI region where resources will be created"
+  type        = string
+}
+
+# general oci parameters ----------------------------------------------------
+variable "compartment_name" {
+  description = "Name for the new compartment"
+  type        = string
+}
+
+variable "compartment_description" {
+  description = "Description of the new compartment"
+  type        = string
+}
+
+variable "compartment_master_ocid" {
+  description = "OCID of the master compartment"
+  type        = string
+}
+
+# compute instance parameters ----------------------------------------------------
+variable "compute_shape" {
+  description = "The shape of compute instance."
+  default     = "VM.Standard.E2.1"
+  type        = string
+}
+
+variable "linux_image_ocid" {
+  type = map
+
+  default = {
+    # See https://docs.us-phoenix-1.oraclecloud.com/images/
+    # Oracle-provided image "Oracle-Linux-7.8-2020.04.17-0"
+    eu-zurich-1     = "ocid1.image.oc1.eu-zurich-1.aaaaaaaa5ganyj57k2dqyik4m4btpuq23le3e7clh56rjhgz6fekvtoyazqa"
+    eu-frankfurt-1  = "ocid1.image.oc1.eu-frankfurt-1.aaaaaaaavz6p7tyrczcwd5uvq6x2wqkbwcrjjbuohbjomtzv32k5bq24rsha"
+    eu-amsterdam-1  = "ocid1.image.oc1.eu-amsterdam-1.aaaaaaaaie5km236l53ymcvpwufyb2srtc3hw2pa6astfjdafzlxxdv5nfsq"
+    us-ashburn-1    = "ocid1.image.oc1.iad.aaaaaaaahjkmmew2pjrcpylaf6zdddtom6xjnazwptervti35keqd4fdylca"
+  }
+}
+
+variable "compute_display_name" {
+  description = "Display name of host"
+  default     = "Application Server - Private Subnet"
+  type        = string
+}
+
+variable "compute_ssh_authorized_keys" {
+  description = "path to the ssh public key file"
+  default     = ""
+  type        = string
+}
+
+
+# openvpnas instance parameters ----------------------------------------------------
+variable "mp_listing_id" {
+  description = "OCI Marketplace Listing ID"
+  default     = "ocid1.appcataloglisting.oc1..aaaaaaaafbgwdxg5j6jnyfhbcxvd62iabcraaf6bwu2u2nhrddztrrle66lq"
+  type        = string
+}
+
+variable "mp_listing_resource_id" {
+  description = "OCI Marketplace Listing Resource ID"
+  default     = "ocid1.image.oc1..aaaaaaaa4ozqggnywlp3e3wzvu5x3aoohkt6cwm2pumgpn2tlzroj756azma"
+  type        = string
+}
+
+variable "mp_listing_resource_version" {
+  description = "OCI Marketplace Listing Version"
+  default     = "AS_2.8.3"
+  type        = string
+}
+
+variable "openvpn_display_name" {
+  description = "Display name of OpenVPN Access Server"
+  default     = "OpenVPN Access Server - Public Subnet"
+  type        = string
+}
+
+variable "openvpn_admin_username" {
+  description = "OpenVPN Application Server username"
+  default     = "openvpnadmin"
+  type        = string
+}
+
+variable "openvpn_admin_password" {
+  description = "OpenVPN Application Server admin password"
+  type        = string
+}
+
+variable "openvpn_activation_key" {
+  description = "OpenVPN Application Server License Activation Key"
+  default     = ""
+  type        = string
+}
+
+# vcn parameters ------------------------------------------------------------
+variable "vcn_name" {
+    description = "name of VCN"
+    default     = "VCN OpenVPN Access"
+    type        = string
+}
+
+variable "vcn_dns_label" {
+    description = "DNS label of VCN"
+    default     = "vcnopenvpnas"
+    type        = string
+}
+
+variable "vcn_cidr" {
+    description = "cidr block of VCN"
+    default     = "10.1.0.0/24"
+    type        = string
+}
+
+variable "vcn_public_cidr_block" {
+    description = "cidr block of public subnet"
+    default     = "10.1.0.0/27"
+    type        = string
+}
+
+variable "vcn_private_cidr_block" {
+    description = "cidr block of private subnet"
+    default     = "10.1.0.32/27"
+    type        = string
+}
+
+variable "nat_gateway_display_name" {
+    description = "Display name of NAT gateway"
+    default     = "ng-01"
+    type        = string
+}
+
+variable "internet_gateway_display_name" {
+    description = "Display name of internet gateway"
+    default     = "ig-01"
+    type        = string
+}
+
+variable "service_gateway_display_name" {
+    description = "Display name of internet gateway"
+    default     = "sg-01"
+    type        = string
+}
+
+variable "service_gateway_enabled" {
+    description = "Service gateway enabled"
+    default     = true
+    type        = bool
+}
+
+variable "public_route_table_display_name" {
+    description = "Display name of the public route table"
+    default     = "rt-public-01"
+    type        = string
+}
+
+variable "private_route_table_display_name" {
+    description = "Display name of the private route table"
+    default     = "rt-private-01"
+    type        = string
+}
+
+# --- EOF -------------------------------------------------------------------
